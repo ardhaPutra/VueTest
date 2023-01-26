6862009f47067d44bf8e731b883e0179794370fa
ardha78 <ardhakrs@gmail.com>
2023-01-26 15:17:18 +0800

    add admin template

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MainLayout from '../layouts/MainLayout.vue'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: MainLayout,
+    children: [
+    {
+      path: '',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: 'about',
+      name: 'about',
+      component: function () {
+        return import('../views/AboutView.vue')
+      }
     }
-  }
+    ]
+  },
 ]
 
 const router = createRouter({
