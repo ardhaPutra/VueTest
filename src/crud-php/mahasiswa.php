a1ae1e3b4b66637e76bfd36bc5aac7a876781d69
ardha78 <ardhakrs@gmail.com>
2023-01-31 00:27:00 +0800

    add html2pdf

--- src/crud-php/mahasiswa.php
+++ src/crud-php/mahasiswa.php
@@ -35,7 +35,9 @@ $data_mahasiswa = select("SELECT * FROM mahasiswa ORDER BY id_mahasiswa DESC");
     
     <a href="tambah-mahasiswa.php" class="btn btn-primary mb-2"><i class="fas fa-plus-circle"></i> Tambah Data</a>
 
-    <a href="download-excel-mahasiswa.php" class="btn btn-dark mb-2"><i class="fas fa-file-excel"></i> Download Excel</a>
+    <a href="download-excel-mahasiswa.php" class="btn btn-success mb-2"><i class="fas fa-file-excel"></i> Download Excel</a>
+
+    <a href="download-pdf-mahasiswa.php" class="btn btn-danger mb-2"><i class="fas fa-file-pdf"></i> Download PDF</a>
     
     <table class="table table-bordered table-striped mt-3" id="table">
         <thead>
